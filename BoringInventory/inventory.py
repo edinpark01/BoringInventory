044ecbf8d9589387894e83626a19442248dc1588
edinpark <tonaco.braulio@gmail.com>
2019-01-17 10:13:51 -0500

    Started inventory.py file

---
+++ BoringInventory/inventory.py
@@ -0,0 +1,21 @@
+import functools
+
+from flask import ( Blueprint, flash, g, redirect, render_template, request, url_for )
+
+
+# TODO: Develop auth.py file
+#from BoringInventory.auth import login_required
+
+# TODO: Develop db.py file
+#from BoringInventory.db import get_db
+
+from werkzeug.exceptions import abort
+
+
+bp = Blueprint('inventory', __name__)
+
+
+@bp.route('/')
+def index():
+    return render_template('inventory/login.html')
+
