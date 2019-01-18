318fff5cd7b729e60992c2ebd083c2effa1c7387
edinpark <tonaco.braulio@gmail.com>
2019-01-18 13:22:55 -0500

    Added some dummy data

--- BoringInventory/schema.sql
+++ BoringInventory/schema.sql
@@ -28,3 +28,19 @@ CREATE TABLE cabinets (
   FOREIGN KEY (cabinet) REFERENCES cabinet_info(id)
 );
 
+INSERT INTO cabinet_info (id, make, model) VALUES ('test1', 'HP', 'zoom3');
+INSERT INTO cabinet_info (id, make, model) VALUES ('test2', 'DELL', 'pinga3');
+INSERT INTO cabinet_info (id, make, model) VALUES ('test3', 'LIMAO', 'caipirinha');
+
+INSERT INTO
+  cabinets (cabinet, location, hostname, manufacturer, model, description, asset_tag, serial, owners, notes)
+VALUES
+       ('test1', 25, 'NPKDSV8001', 'HP', 'HP ProLiant', 'Oh boy', '2011234', 'ADV123', 'John Doe', 'Swithing owners soon');
+INSERT INTO
+  cabinets (cabinet, location, hostname, manufacturer, model, description, asset_tag, serial, owners)
+VALUES
+       ('test2', 25, 'NPKDSV8002', 'Panasonic', 'Lumen', 'Oh girl', '2011232', 'TGH713', 'Mary Doe');
+INSERT INTO
+  cabinets (cabinet, location, hostname, manufacturer, model, description, asset_tag, serial, owners)
+VALUES
+       ('test3', 25, 'NPKDSV8007', 'Cisco', 'Cabecao', 'Oh guys', '20145634', 'EFD456', 'Junior Doe');
