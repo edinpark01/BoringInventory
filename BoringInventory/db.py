91786dbb59efaa9bdca49a772976749f2052f8f5
edinpark <tonaco.braulio@gmail.com>
2019-01-18 12:21:10 -0500

    Finished, for now, with db.py file

--- BoringInventory/db.py
+++ BoringInventory/db.py
@@ -38,3 +38,41 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+        # open_resource():
+        #   Opens a file relative to the BoringInventory package, which is useful since we won't necessarily know where
+        #   that location is when deploying the application later.
+
+
+# click.command():
+#   defines a command line command called init-db that calls the init_db function and shows a success message to the
+#   user.
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """ Clear existing data and create new tables. """
+    init_db()
+    click.echo(" * Database has been initialized")
+
+
+# The close_db and init_db_command functions need to be registered with the application instance, otherwise they won't
+# be used by the application. However, since we are using a factory function, that instance is not available when
+# writing function. Instead, write a function that takes an application and does the registration.
+def init_app(app):
+
+    click.echo(" * Registering close_db and init_db_command with app")
+
+    app.teardown_appcontext(close_db)
+    # app.teardown_appcontext():
+    #   tells Flaks to call that function when cleaning up after returning the response
+
+    app.cli.add_command(init_db_command)
+    # app.cli.add_command():
+    #   adds a new command that can be called with the flask command
+
